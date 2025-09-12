4404b5d8ebc2ebe30164bb071d2ad514b11787c6
Brunogonzalez00 <a2021113701@estudiantes.upsa.edu.bo>
2025-09-12 14:59:40 -0400

    Fixes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect} from 'react';
 import { ArrowUpCircle, ArrowDownCircle, TrendingUp, CalendarDays, Plus, Brain } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useTransactionStore } from '../stores/transactionStore';
